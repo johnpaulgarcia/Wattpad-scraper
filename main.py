22d7a01cb6c28e268dc4896fee158a3bcec58eae
john.garcia <john.garcia@boom.camp>
2021-09-30 09:37:31 +0800

    test

--- main.py
+++ main.py
@@ -32,4 +32,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8059, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
